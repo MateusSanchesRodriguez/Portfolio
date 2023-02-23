ef6dc1c1a08b2b69adeb72a7d959339c54d334ab
Mateus Sanches <mateussanchesrodriguez@gmail.com>
2023-02-22 22:49:04 -0300

    Att sobre mim, meu curriculo e subindo projetos

--- Js/index.js
+++ Js/index.js
@@ -3,7 +3,6 @@ gsap.registerPlugin(Flip);
 const links = document.querySelectorAll(".nav-item a");
 const activeNav = document.querySelector(".active-nav");
 
-
 links.forEach((link) => {
   link.addEventListener("click", () => {
     const state = Flip.getState(activeNav);
@@ -23,8 +22,10 @@ projects.forEach((project, index) => {
     const state = Flip.getState(projects);
 
     const isProjectActive = project.classList.contains("active");
-    projects.forEach((otherProjects, otherIndex) => {
-      otherProjects.classList.remove("active");
+    projects.forEach((otherProjects, otherIndex, e) => {
+      if (window.event.target.className !== "siteLinks" && window.event.target.className !== "githubLinks" && window.event.target.className !== "imgProject") {
+        otherProjects.classList.remove("active");
+      }
       otherProjects.classList.remove("is-inactive");
       if (!isProjectActive && index !== otherIndex) {
         otherProjects.classList.add("is-inactive");
